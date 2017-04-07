a52a5f6262c11b8094e0450bae165d2f84915b69
YiwenWang <782252996@qq.com>
2017-04-07 13:36:03 +0800

    修正击杀怪物后仍能在怪物单元格执行战斗命令的BUG，并删除了一些不需要的资源

---
+++ out/test.js
@@ -0,0 +1,24 @@
+"use strict";
+var canvas = document.getElementById("app");
+var stage = engine.run(canvas);
+var touchService = engine.TouchEventService.getInstance();
+// var bitmap = new engine.Bitmap();
+// engine.RES.getRes("0008.png").then((value) => {
+//     bitmap.texture = value;
+//     bitmap.setWidth(bitmap.texture.width);
+//     bitmap.setHeight(bitmap.texture.height);
+// });
+// stage.addChild(bitmap);
+// let speed = 190;
+// engine.Ticker.getInstance().register((deltaTime) => {
+//     //console.log("aaa");
+//     bitmap.setWidth(500);
+// });
+engine.RES.getRES("RES.json", (data) => {
+    var preloadJson = data;
+    engine.RES.loadConfig(preloadJson, () => {
+        console.log("load complete!!!");
+        var main = new Main(stage, touchService);
+        main.createGameScene();
+    });
+});
